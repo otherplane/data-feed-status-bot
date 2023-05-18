613d307f4dffcedc6b7f6c8b29b64a53f2f5b8eb
tommy <tomastrg22@gmail.com>
2023-05-18 10:28:13 +0200

    feat: add 'one' as mainnet keyword

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const MAINNET_KEYWORDS = ['mainnet', 'tethys']
+export const MAINNET_KEYWORDS = ['mainnet', 'tethys', 'one']
 
 export const ADMISSIBLE_DELAY_MS: number =
   Number(process.env.ADMISSIBLE_DELAY_SECONDS || '0') * 1000

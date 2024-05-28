50da231c1492e31cc97c7a0669e4a2c62ea94170
tommytrg <tomastrg22@gmail.com>
2024-05-28 16:38:26 +0200

    chore: consider moonriver mainnet

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const MAINNET_KEYWORDS = ['mainnet', 'tethys', 'one']
+export const MAINNET_KEYWORDS = ['mainnet', 'tethys', 'one', 'moonriver']
 
 export const ADMISSIBLE_DELAY_MS: number =
   Number(process.env.ADMISSIBLE_DELAY_SECONDS || '0') * 1000
